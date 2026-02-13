3deb9eaf144983011e6c9fe48d662f15ea0ce3e5
Shahryar <shahryar.saleem007@gmail.com>
2026-02-12 19:14:07 -0500

    updated pageurls test

--- tests/Playwright Basic Actions/plawright-actions-pageurls.spec.ts
+++ tests/Playwright Basic Actions/plawright-actions-pageurls.spec.ts
@@ -3,6 +3,8 @@ test('Get page URL and validate it with actual', async({page})=>{
 await page.goto('https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php');
 //get page url
 const pageURL = page.url();
+//print page url
+console.log(pageURL);
 //assert of page url
-expect(page).toHaveURL(/automation_practice.php/);
+expect(pageURL).toMatch(/automation_practice.php/);
 });
