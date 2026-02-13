ccbc5b53bc733361ba76b7f87ae30c7698929c15
Shahryar <shahryar.saleem007@gmail.com>
2026-02-12 19:21:23 -0500

    updated pageurls test

--- tests/Playwright Basic Actions/plawright-actions-pageurls.spec.ts
+++ tests/Playwright Basic Actions/plawright-actions-pageurls.spec.ts
@@ -1,4 +1,6 @@
 import {test, expect} from '@playwright/test';
+import { describe } from 'node:test';
+describe('Method page.url()')
 test('Get page URL and validate it with actual', async({page})=>{
 await page.goto('https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php');
 //get page url
@@ -7,4 +9,12 @@ const pageURL = page.url();
 console.log(pageURL);
 //assert of page url
 expect(pageURL).toMatch(/automation_practice.php/);
+});
+
+//toHaveURL method
+describe ('Method 2 .toHaveURL')
+test('Get page URL validate it with actual using .toHaveURL method', async({page})=>{
+await page.goto('https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php');
+//assert of page url
+await expect(page).toHaveURL(/automation_practice.php/);
 });
